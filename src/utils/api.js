2415abe4bb9a764cb4c1c3405c36362aaa500924
Sakshi Ganeshkar <sakshiganeshkar21@gmail.com>
2026-04-12 14:00:44 +0530

    remove api from base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://study-planer-backend.vercel.app/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://study-planer-backend.vercel.app',
   timeout: 10000,
 });
 
