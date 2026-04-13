887f5b04de92d27411369fb43f52d089f6b900ea
Sakshi Ganeshkar <sakshiganeshkar21@gmail.com>
2026-04-13 23:25:15 +0530

    fix: node version

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://study-planer-backend-f9f7.vercel.app/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://study-planer-backend.vercel.app/api',
   timeout: 10000,
 });
 
